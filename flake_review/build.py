e12e2a33e530ef33ce5ef053ceaddfe44f35c3cb
Josef Hofer <me@jhofer.de>
2026-02-19 16:05:22 +0100

    ops: improve pipeline speed?

--- flake_review/build.py
+++ flake_review/build.py
@@ -69,6 +69,8 @@ def build_derivation(
                 f"{flake_path}#{derivation.attr_path}",
                 "--no-link",
                 "--print-out-paths",
+                "--substituters",
+                "https://cache.nixos.org",
             ],
             check=False,
         )
